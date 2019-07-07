c8153e153b0dc55cd10b8d6e46c290a2c9ef857b
ambrish018 <incredible.bil@gmail.com>
2019-07-07 15:58:14 +0530

    Initial Commit

---
+++ FairPlaySDK/FairPlaySDK.swift
@@ -0,0 +1,11 @@
+//
+//  FairPlaySDK.swift
+//  FairPlaySDK
+//
+//  Created by YASH COMPUTERS on 07/07/19.
+//  Copyright © 2019 YASH COMPUTERS. All rights reserved.
+//
+
+class FairPlaySDK {
+
+}
